bc994a768d843da6707c8601c94089a90ed0611b
Andrea <35015674+Sgridev@users.noreply.github.com>
2020-05-22 01:25:17 +0200

    fixed news anchor bug + beautify code

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,35 +8,34 @@ $(document).ready(function() {
         $(this).find('.dropdown-content').stop(true, true).delay(200).fadeOut();
     });
     AOS.init();
-    window.onscroll = function() {scrollFunction()};
+    window.onscroll = function() { scrollFunction() };
 
     function scrollFunction() {
-      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#navbar").css({'height':'65px'});
-        $("#logo").css({'height':'55px'});
-        $("#logo").css({'width':'55px'});
-        $('.navbar-toggler').css({'margin-top':'10%'});
-      }
-       else {
-        $("#navbar").css({'height':'100px'});
-        $("#logo").css({'height':'90px'});
-        $("#logo").css({'width':'90px'});
-        $('.navbar-toggler').css({'margin-top':'40%'});
-      }
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            $("#navbar").css({ 'height': '65px' });
+            $("#logo").css({ 'height': '55px' });
+            $("#logo").css({ 'width': '55px' });
+            $('.navbar-toggler').css({ 'margin-top': '10%' });
+        } else {
+            $("#navbar").css({ 'height': '100px' });
+            $("#logo").css({ 'height': '90px' });
+            $("#logo").css({ 'width': '90px' });
+            $('.navbar-toggler').css({ 'margin-top': '40%' });
+        }
     }
     document.querySelectorAll('a[href^="/"]').forEach(anchor => {
-        if(anchor.id.includes("nav") && (location.pathname == "/" || location.pathname.includes("index.html"))){
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            //id of <a> in nav must be "nav+idsection"
-            $("#" + this.id.substring(3))[0].scrollIntoView({
-                behavior: 'smooth'
+        if (anchor.id.includes("nav") && (location.pathname == "/" || location.pathname.includes("index.html"))) {
+            anchor.addEventListener('click', function(e) {
+                e.preventDefault();
+                //id of <a> in nav must be "nav+idsection"
+                $("#" + this.id.substring(3))[0].scrollIntoView({
+                    behavior: 'smooth'
+                });
             });
-        });
-    }
+        }
     });
 });
-$(window).on("load",function(){
+$(window).on("load", function() {
     $(".loader-wrapper").fadeOut("slow");
-    $(".navbar").css({"display": "block"});
-  });
+    $(".navbar").css({ "display": "block" });
+});
